b933b1f652489b58297385daf2b760d8556937ed
tzarhs <j.tzaris@gmail.com>
2025-11-07 11:36:09 +0200

    fixing Render error

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   // Express will serve up the index.html file if it doesn't recognize the route
   const path = require("path");
-  app.all("*", (req, res) => {
+  app.get((req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
